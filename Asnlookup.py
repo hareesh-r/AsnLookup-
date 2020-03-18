64433a5cff5047807e4e33cc9fe868de71e26266
abisheikMagesh <abireena2002@gmail.com>
2020-03-18 10:55:56 +0530

    Update Asnlookup.py

--- Asnlookup.py
+++ Asnlookup.py
@@ -1,6 +1,7 @@
+import urllib.request
 import urllib as url
-print("          ASN lookup \n"+" \n ▁ ▂ ▄ ▅ ▆ ▇ █ 𝟘𝕩𝟞𝟙𝟞𝕓𝟞𝕕 █ ▇ ▆ ▅ ▄ ▂ ▁"+" \n \n follow me on instagram :"+"https://www.instagram.com/__its_just._.akm/")
+print("          ASN lookup \n"+"Created By :"+"Abisheik M \n"+" \n ▁ ▂ ▄ ▅ ▆ ▇ █ 𝟘𝕩𝟞𝟙𝟞𝕓𝟞𝕕 █ ▇ ▆ ▅ ▄ ▂ ▁"+" \n \n follow me on instagram :"+"https://www.instagram.com/__its_just._.akm/")
 Asn = input("Enter the Asn to Get the Ip Range : ") #Iknow the code suck but i can give you output
 web = url.request.urlopen("https://api.hackertarget.com/aslookup/?q=" + Asn)
-data = web.read()
-print(data)
+data = list(str(web.read()).split("n"))
+for i in data:print(str(i)[:-1])
