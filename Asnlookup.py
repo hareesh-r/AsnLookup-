143607b5688858d8593976eb2af241dc637c1bc1
abisheikMagesh <abireena2002@gmail.com>
2020-03-18 10:27:04 +0530

    Update Asnlookup.py

--- Asnlookup.py
+++ Asnlookup.py
@@ -1,6 +1,6 @@
 import urllib as url
 print("          ASN lookup \n"+" \n ▁ ▂ ▄ ▅ ▆ ▇ █ 𝟘𝕩𝟞𝟙𝟞𝕓𝟞𝕕 █ ▇ ▆ ▅ ▄ ▂ ▁"+" \n \n follow me on instagram :"+"https://www.instagram.com/__its_just._.akm/")
 Asn = input("Enter the Asn to Get the Ip Range : ") #Iknow the code suck but i can give you output
-web = urllib.request.urlopen("https://api.hackertarget.com/aslookup/?q=" + Asn)
+web = url.request.urlopen("https://api.hackertarget.com/aslookup/?q=" + Asn)
 data = web.read()
 print(data)
